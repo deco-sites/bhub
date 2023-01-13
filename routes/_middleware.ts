89f1facde23fc5ee797c85758783ec2d139d4880
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2023-01-13 17:17:38 -0300

    [Automatic] Live site update

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,7 +1,7 @@
 import { withLive } from "$live/live.ts";
 
 export const handler = withLive({
-  site: "start",
-  siteId: 8,
-  domains: [],
+  siteId: 461,
+  site: "bhub",
+  domains: ["deco-sites-bhub.deno.dev"],
 });
